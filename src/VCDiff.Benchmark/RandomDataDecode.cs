4f0d92f86f0b66bb612de7502770fe48bf788370
Sewer 56 <seweratwork@gmail.com>
2021-10-02 02:14:54 +0100

    Added: Basic (Unrealistic) Benchmark for Encoding/Decoding

---
+++ src/VCDiff.Benchmark/RandomDataDecode.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+using VCDiff.Decoders;
+using VCDiff.Encoders;
+
+namespace VCDiff.Benchmark
+{
+    [SimpleJob(1, 3, 8)]
+    public class RandomDataDecode
+    {
+        [Params(//1 * 1024 * 256,  // 0.25MiB
+            //1 * 1024 * 1024, // 2 MiB
+            16 * 1024 * 1024 // 16 MiB
+        )]
+        public int Bytes { get; set; }
+
+        [Params(32)] // AVX Only
+        //[Params(16, 32)] // AVX vs SSE
+        public int BlockSize { get; set; }
+
+        private const int RepeatCount = 128;
+        private byte[] _data;
+        private Stream _patchSlightModified;
+        private Stream _patchHeavyModified;
+
+        private Stream _sourceStream;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _data = RandomDataGenerator.GetRandomBytes(Bytes);
+            RandomDataEncode.MakeRandomData(_data, out var dataSlightModified, out var dataHeavyModified);
+
+            // Make Source Stream
+            _sourceStream = new MemoryStream(_data);
+
+            // Make Slightly Mod Patch
+            CreatePatch(dataSlightModified, ref _patchSlightModified);
+            CreatePatch(dataHeavyModified, ref _patchHeavyModified);
+        }
+
+        private void CreatePatch(byte[] targetData, ref Stream receiver)
+        {
+            _sourceStream.Seek(0, SeekOrigin.Begin);
+            receiver = new MemoryStream(_data.Length);
+            using var targetStream = new MemoryStream(targetData);
+            using var encoder = new VcEncoder(_sourceStream, targetStream, receiver, 1, BlockSize);
+            encoder.Encode();
+        }
+        
+        [Benchmark]
+        public void DecodeSlightlyModified()
+        {
+            using var result = new MemoryStream((int)_sourceStream.Length);
+            for (int x = 0; x < RepeatCount; x++)
+            {
+                _sourceStream.Seek(0, SeekOrigin.Begin);
+                _patchSlightModified.Seek(0, SeekOrigin.Begin);
+                result.Position = 0;
+                using var decoder = new VcDecoder(_sourceStream, _patchSlightModified, result, int.MaxValue);
+                decoder.Decode(out long written);
+            }
+        }
+
+        [Benchmark]
+        public void DecodeHeavilyModified()
+        {
+            using var result = new MemoryStream((int)_sourceStream.Length);
+            for (int x = 0; x < RepeatCount; x++)
+            {
+                _sourceStream.Seek(0, SeekOrigin.Begin);
+                _patchHeavyModified.Seek(0, SeekOrigin.Begin);
+                result.Position = 0;
+                using var decoder = new VcDecoder(_sourceStream, _patchHeavyModified, result, int.MaxValue);
+                decoder.Decode(out long written);
+            }
+        }
+    }
+}
---
+++ src/VCDiff.Benchmark/RandomDataEncode.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+using BenchmarkDotNet.Attributes;
+using VCDiff.Encoders;
+
+namespace VCDiff.Benchmark
+{
+    [SimpleJob(1, 3, 8)]
+    public class RandomDataEncode
+    {
+        [Params(//1 * 1024 * 256,  // 0.25MiB
+                //1 * 1024 * 1024, // 2 MiB
+                16 * 1024 * 1024 // 16 MiB
+                )]
+        public int Bytes { get; set; }
+
+        [Params(32)] // AVX Only
+        //[Params(16, 32)] // AVX vs SSE
+        public int BlockSize { get; set; }
+
+        private byte[] _data;
+        private byte[] _dataSlightModified;
+        private byte[] _dataHeavyModified;
+
+        private Stream _sourceStream;
+
+        [GlobalSetup]
+        public void GlobalSetup()
+        {
+            _data = RandomDataGenerator.GetRandomBytes(Bytes);
+            MakeRandomData(_data, out _dataSlightModified, out _dataHeavyModified);
+            _sourceStream = new MemoryStream(_data);
+        }
+
+        [IterationSetup]
+        public void IterationSetup() => _sourceStream.Seek(0, SeekOrigin.Begin);
+
+        [Benchmark]
+        public void EncodeSlightlyModified()
+        {
+            using var targetStream = new MemoryStream(_dataSlightModified);
+            using var patchStream  = new MemoryStream(_data.Length);
+
+            using var encoder = new VcEncoder(_sourceStream, targetStream, patchStream, 1, BlockSize);
+            encoder.Encode();
+        }
+
+        [Benchmark]
+        public void EncodeHeavilyModified()
+        {
+            using var targetStream = new MemoryStream(_dataHeavyModified);
+            using var patchStream  = new MemoryStream(_data.Length);
+
+            using var encoder = new VcEncoder(_sourceStream, targetStream, patchStream, 1, BlockSize);
+            encoder.Encode();
+        }
+
+
+        // Utility Methods
+        public static void MakeRandomData(byte[] data, out byte[] dataSlightModified, out byte[] dataHeavyModified)
+        {
+            dataSlightModified = (byte[])data.Clone();
+            dataHeavyModified = (byte[])data.Clone();
+
+            var random = new Random(data.Length);
+            for (int x = 0; x < dataHeavyModified.Length; x++)
+            {
+                var next = random.Next(0, 1000);
+                if (next >= 250) // 3 / 4 chance.
+                    dataHeavyModified[x] += (byte)next;
+                if (next >= 995) // 1 / 200 chance.
+                    dataSlightModified[x] += (byte)next;
+            }
+        }
+    }
+}
