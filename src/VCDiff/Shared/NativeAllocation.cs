cbde3ae06faab817bc2cb12e31faf50ad06d394b
Sewer 56 <seweratwork@gmail.com>
2021-10-02 03:47:19 +0100

    Optimized: Code Tables

--- src/VCDiff/Shared/NativeAllocation.cs
+++ src/VCDiff/Shared/NativeAllocation.cs
@@ -3,32 +3,32 @@ using System.Runtime.InteropServices;
 
 namespace VCDiff.Shared
 {
-    internal struct NativeAllocation : IDisposable
+    internal unsafe struct NativeAllocation : IDisposable
     {
-        public IntPtr Address;
+        public byte* Pointer;
         public int Size;
         public bool OwnsAllocation;
 
         public NativeAllocation(int size)
         {
-            Address = Marshal.AllocHGlobal(size);
+            Pointer = (byte*) Marshal.AllocHGlobal(size);
             Size = size;
             OwnsAllocation = true;
         }
 
         public NativeAllocation(IntPtr address, int size) : this()
         {
-            Address = address;
+            Pointer = (byte*) address;
             Size = size;
             OwnsAllocation = false;
         }
 
-        public unsafe Span<byte> AsSpan() => new Span<byte>((void*) Address, Size);
+        public unsafe Span<byte> AsSpan() => new Span<byte>((void*) Pointer, Size);
 
         public void Dispose()
         {
-            if (Address != IntPtr.Zero)
-                Marshal.FreeHGlobal(Address);
+            if (Pointer != (void*)0)
+                Marshal.FreeHGlobal((IntPtr) Pointer);
         }
     }
 }
