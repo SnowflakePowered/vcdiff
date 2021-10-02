f863cc09ee45da4d3cdc996c8ce958ec82d1178d
Sewer 56 <seweratwork@gmail.com>
2021-10-02 02:14:54 +0100

    Added: Basic (Unrealistic) Benchmark for Encoding/Decoding

---
+++ src/VCDiff.Benchmark/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using BenchmarkDotNet.Running;
+
+namespace VCDiff.Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkRunner.Run<RandomDataDecode>();
+            BenchmarkRunner.Run<RandomDataEncode>();
+        }
+    }
+}
---
+++ src/VCDiff.Benchmark/RandomDataGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VCDiff.Benchmark
+{
+    /// <summary>
+    /// Generates random data.
+    /// </summary>
+    public static class RandomDataGenerator
+    {
+        public static byte[] GetRandomBytes(int size)
+        {
+            var data = new byte[size];
+            var random = new Random(size);
+            for (int x = 0; x < data.Length; x++)
+                data[x] = (byte)random.Next();
+
+            return data;
+        }
+    }
+}
